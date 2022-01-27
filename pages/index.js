f6fbd3a7388d9108a2debb88795a3faa0648746f
{ Walyson } <walysonmoura222@gmail.com>
2022-01-27 16:28:23 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function PaginaInicial() {
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          backgroundImage: 'url(https://i.ibb.co/dGT8J2n/images-9.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
