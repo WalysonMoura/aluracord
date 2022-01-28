d7140c42862da584dc3d65ad5f819edd03208343
{ Walyson } <walysonmoura222@gmail.com>
2022-01-28 11:48:17 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -43,7 +43,7 @@ export default function PaginaInicial() {
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
           backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://i.ibb.co/dGT8J2n/images-9.jpg)',
+          backgroundImage: 'url(https://i.ibb.co/wSHZcbc/20220128-112041.gif)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
