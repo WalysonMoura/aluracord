094a83892d572e4472082127c267b7b6d3d878a0
{ Walyson } <walysonmoura222@gmail.com>
2022-01-29 13:52:13 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -77,7 +77,7 @@ export default function PaginaInicial() {
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas de ta!</Titulo>
+            <Titulo tag="h2">Bem vindo (a)👋!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
